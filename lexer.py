06ba69de075558dcbc75b319018f972a64857851
duongoku <nguyenvubinhduong@gmail.com>
2023-03-17 14:59:50 +0700

    Add docs

--- lexer.py
+++ lexer.py
@@ -13,13 +13,48 @@ EPILOG = "this is a part of the VC compiler project | author: duongoku"
 def read_file(path: str):
     with open(path, "r") as file:
         return file.read()
-    
+
 def check_match(match: str, char: str):
+    """
+    Parameters
+    ----------
+    match : str
+        a string of characters to match
+    char : str
+        the character to check
+
+    Returns
+    -------
+    bool
+        True if the match contains the character, False otherwise
+    """
     if match.startswith(EXCLUDE):
         return char not in match[len(EXCLUDE):]
     return char in match
 
+#
 def lexer(source: str, nodes: dict, keywords: list, special_literals: list, separators: str):
+    """
+    Parameters
+    ----------
+    source : str
+        the source code to parse
+    nodes : dict
+        the nodes in the DFA
+    keywords : list
+        the list of keywords
+    special_literals : list
+        the list of special literals
+    separators : str
+        the list of separators
+
+    Returns
+    -------
+    list
+        a list of tokens
+    """
+
+    # Initialize the starting state
     STARTING_STATE = "0"
     for id in nodes:
         if nodes[id]["starting"]:
@@ -40,6 +75,7 @@ def lexer(source: str, nodes: dict, keywords: list, special_literals: list, sepa
         index += 1
         position += 1
         
+        # Skip whitespaces if the state is the starting state
         if state == STARTING_STATE and char in WHITESPACES:
             if char not in NEWLINE:
                 new_line_stack = ""
@@ -51,7 +87,8 @@ def lexer(source: str, nodes: dict, keywords: list, special_literals: list, sepa
                 new_line_stack = ""
                 line -= 1
             continue
-
+        
+        # Check if current character matches any of the children
         children = nodes[state]["children"]
         found = False
         for match in children.keys():
@@ -62,10 +99,13 @@ def lexer(source: str, nodes: dict, keywords: list, special_literals: list, sepa
                 token += char
                 found = True
                 break
-            
+        
+        # If no match is found, check if the current state is a terminal state
         if not found:
             index -= 1
             position -= 1
+
+            # If the current state is a terminal state, add the token to the list of tokens
             if nodes[state]["terminal"] and (char in separators or token[-1] in separators):
                 if token in keywords:
                     tokens.append({"token": token, "type": "KEYWORD", "line": line, "start": start, "end": position})
@@ -78,6 +118,8 @@ def lexer(source: str, nodes: dict, keywords: list, special_literals: list, sepa
                     position = 0
                 token = ""
                 state = STARTING_STATE
+
+            # If the current state is not a terminal state, print an error message
             else:
                 error_msg = f"Error while parsing '{token}': invalid character at line {line}({position}): '{char}', "
                 expecting = list(nodes[state]['children'].keys())
@@ -95,7 +137,8 @@ def lexer(source: str, nodes: dict, keywords: list, special_literals: list, sepa
                 print(error_msg)
                 token = ""
                 state = STARTING_STATE
-            
+
+    # Add the last token to the list of tokens
     if token:
         if nodes[state]["terminal"]:
             if token in keywords:
@@ -108,6 +151,7 @@ def lexer(source: str, nodes: dict, keywords: list, special_literals: list, sepa
     return tokens
 
 if __name__ == "__main__":
+    # Parse the command line arguments
     parser = argparse.ArgumentParser(
         prog=NAME,
         description=DESCRIPTION,
@@ -122,6 +166,7 @@ if __name__ == "__main__":
     datafile = args.datafile
     no_comments = args.no_comments
 
+    # Read the source code and data file containing the DFA
     source = read_file(filename)
     with open(datafile, "r") as file:
         data = json.load(file)
@@ -131,16 +176,18 @@ if __name__ == "__main__":
         TOKEN_TYPES = data["terminal_types"]
         nodes = data["nodes"]
 
+    # Parse the source code
     print("Parsing file: " + filename)
     start = time.time()
     result = lexer(source, nodes, KEYWORDS, SPECIAL_LITERALS, SEPARATORS)
     end = time.time()
     print(f"Done in {end-start:.3f} seconds.")
 
-    
+    # Remove comments if the user specified the -n or --no-comments option
     if no_comments:
         result = [token for token in result if token["type"] != "COMMENT"]
 
+    # Export the tokens
     verbose = "======= The VC compiler ======="
     for token in result:
         verbose += f"\nKind = {TOKEN_TYPES.index(token['type'])} [{token['type']}]"
